5e9824fe7716ef560d3c6a73e901892e5eb9e37f
TommyMynnSon <tommy.m.son@gmail.com>
2023-09-06 22:55:24 -0700

    Add nav styling for +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,20 @@
 
 <nav>
 	<a href="/">SvelteFix</a>
-
 	<div class="links">
 		<a href="/search">Search</a>
 		<a href="/watchlist">Watchlist</a>
 		<a href="/login">Login</a>
 	</div>
 </nav>
+
+<style>
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		width: 100%;
+		height: 3rem;
+	}
+</style>
