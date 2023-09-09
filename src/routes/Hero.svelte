bb35b0c945abca5347b03e9dca2df7b907102add
TommyMynnSon <tommy.m.son@gmail.com>
2023-09-09 15:40:57 -0700

    Render featured movie logo in Hero component

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -6,6 +6,7 @@
 
 	$: images = movie.images;
 	$: backdrop = images.backdrops.find((image) => !image.iso_639_1) || images.backdrops[0];
+	$: logo = images.logos.find((image) => image.iso_639_1 === 'en') || images.backdrops[0];
 </script>
 
 <a href="/movie/{movie.id}">
@@ -15,6 +16,8 @@
 		src={media(backdrop.file_path, 1280)}
 		style="aspect-ratio: {backdrop.aspect_ratio}"
 	/>
+
+	<img alt={movie.title} src={media(logo.file_path, 1280)} />
 </a>
 
 <style>
