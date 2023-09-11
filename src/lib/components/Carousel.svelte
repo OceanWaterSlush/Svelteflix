6ae993d58674d257b5ab5a3ac45e6e145a7ba585
TommyMynnSon <tommy.m.son@gmail.com>
2023-09-11 12:04:22 -0700

    Add Carousel component

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { MovieListResult } from '$lib/types';
+	import { media } from '$lib/api';
+
+	export let movies: MovieListResult[];
+</script>
+
+<div class="carousel">
+	{#each movies as movie}
+		<a href="/movie/{movie.id}">
+			<img alt={movie.title} src={media(movie.poster_path, 500)} />
+		</a>
+	{/each}
+</div>
