a21dc22ead6a490f02b9a1457ef2e7d5b5c9df6e
TommyMynnSon <tommy.m.son@gmail.com>
2023-09-11 22:21:11 -0700

    Use Promise.all to reduce waterfall

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,13 +3,15 @@ import { TMDB_API_KEY } from '$env/static/private';
 import type { MovieDetails, MovieList } from '$lib/types';
 
 export async function load({ fetch }) {
-	const trending = (await api.get(fetch, 'trending/movie/day', {
-		api_key: TMDB_API_KEY
-	})) as MovieList;
-	const now_playing = (await api.get(fetch, 'movie/now_playing', {
-		api_key: TMDB_API_KEY
-	})) as MovieList;
-	const upcoming = (await api.get(fetch, 'movie/upcoming', { api_key: TMDB_API_KEY })) as MovieList;
+	const [trending, now_playing, upcoming] = await Promise.all([
+		(await api.get(fetch, 'trending/movie/day', {
+			api_key: TMDB_API_KEY
+		})) as MovieList,
+		(await api.get(fetch, 'movie/now_playing', {
+			api_key: TMDB_API_KEY
+		})) as MovieList,
+		(await api.get(fetch, 'movie/upcoming', { api_key: TMDB_API_KEY })) as MovieList
+	]);
 
 	const featured = (await api.get(fetch, `movie/${trending.results[0].id}`, {
 		append_to_response: 'images',
