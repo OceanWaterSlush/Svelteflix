d1d6b9214318e1fdc683bff0857b9b046de5a5e8
TommyMynnSon <tommy.m.son@gmail.com>
2023-09-14 23:47:47 -0700

    Prepare ResultsPage for infinite scrolling

--- src/lib/components/ResultsPage.svelte
+++ src/lib/components/ResultsPage.svelte
@@ -6,15 +6,21 @@
 	export let next: string | null;
 </script>
 
-<div class="results">
-	{#each movies as movie}
-		<a href="/movies/{movie.id}">
-			<img alt={movie.title} src={media(movie.poster_path, 500)} />
-		</a>
-	{/each}
+<div class="viewport" style="height: 500px">
+	<div class="results">
+		{#each movies as movie}
+			<a href="/movies/{movie.id}">
+				<img alt={movie.title} src={media(movie.poster_path, 500)} />
+			</a>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.viewport {
+		overflow-y: auto;
+	}
+
 	.results {
 		display: grid;
 		grid-template-columns: repeat(4, 1fr);
